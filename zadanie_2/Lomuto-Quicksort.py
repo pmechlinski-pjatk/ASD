d16e57130a46c3af469ba5178a27834ceaf8dc7c
Paweł Mechliński <s26129@pjwstk.edu.pl>
2023-04-28 21:46:35 +0200

    add extra comments

--- zadanie_2/Lomuto-Quicksort.py
+++ zadanie_2/Lomuto-Quicksort.py
@@ -1,19 +1,26 @@
 import numpy as np
+# Za Cormen, Thomas H.; Leiserson, Charles E.; Rivest, Ronald L.; Stein, Clifford (2009) 
+# Sorts a (portion of an) array, divides it into partitions, then sorts those
 
 def quicksort(arr, lo, hi):
-    if lo < hi:
+    # Ensure indices are in correct order
+    if lo >= hi or lo < 0:
+        return 0
+    elif lo < hi:
         p = partition(arr, lo, hi)
+
+        #  Sort partitions reqursively
         quicksort(arr, lo, p - 1)
         quicksort(arr, p + 1, hi)
 
 def partition(arr, lo, hi):
-    pivot = arr[hi]
+    pivot = arr[hi] # Last element as the pivot
     i = lo
     for j in range(lo, hi):
         if arr[j] < pivot:
-            arr[i], arr[j] = arr[j], arr[i]
+            arr[i], arr[j] = arr[j], arr[i] # Swap
             i += 1
-    arr[i], arr[hi] = arr[hi], arr[i]
+    arr[i], arr[hi] = arr[hi], arr[i] # swap
     return i
 
 
