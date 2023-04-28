437f248fa61758dae224fe07fae1078120b6fccd
Paweł Mechliński <s26129@pjwstk.edu.pl>
2023-04-28 21:56:20 +0200

    doublecheck wip

---
+++ zadanie_2/Lomuto-Quicksort step-by-step.py
@@ -0,0 +1,44 @@
+import numpy as np
+# Za Cormen, Thomas H.; Leiserson, Charles E.; Rivest, Ronald L.; Stein, Clifford (2009) 
+# Sorts a (portion of an) array, divides it into partitions, then sorts those
+
+def quicksort(arr, lo, hi):
+    # Ensure indices are in correct order
+    if lo >= hi or lo < 0:
+        return 0
+    elif lo < hi:
+        p = partition(arr, lo, hi)
+
+        #  Sort partitions reqursively
+        quicksort(arr, lo, p - 1)
+        quicksort(arr, p + 1, hi)
+
+def partition(arr, lo, hi):
+    pivot = arr[hi] # Last element as the pivot
+    i = lo
+    for j in range(lo, hi):
+        if arr[j] < pivot:
+            print("[Swap] "+str(arr[i])+" >< "+str(arr[j]))
+            arr[i], arr[j] = arr[j], arr[i] # Swap
+            i += 1
+    arr[i], arr[hi] = arr[hi], arr[i] # swap
+    return i
+
+def partition_doublecheck(arr, lo, hi):
+    pivot = arr[hi] # Last element as the pivot
+    i = lo
+    for j in range(lo, hi):
+        if arr[j] < pivot and arr[i] != arr[j]:
+            print("[Swap] "+str(arr[i])+" >< "+str(arr[j]))
+            arr[i], arr[j] = arr[j], arr[i] # Swap
+            i += 1
+    arr[i], arr[hi] = arr[hi], arr[i] # swap
+    return i
+
+
+if __name__ == "__main__":
+    A = np.random.randint(-100,100,10)
+    low, high = 0, len(A)-1
+    print("BEFORE SORTING:", A)
+    quicksort(A, low, high)
+    print("\nAFTER SORTING:", A)
